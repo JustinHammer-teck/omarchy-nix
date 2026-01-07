deaca2a783749a4ede5065be3c67629bc3e02730
Moritz Zimmerman <dinhnhattai.nguyen@hotmail.com>
2026-01-07 22:04:13 +1100

    [Update] update git configuration and nixos audio system

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -17,8 +17,10 @@ in
   services.pipewire = {
     enable = true;
     alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.enable = true;
   };
 
   # Initial login experience
